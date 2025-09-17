9e68340a620665c1e7c246743dfe4ddfe0a3c369
sudiptodas153 <sudiptodas4093@gmail.com>
2025-09-18 02:30:05 +0600

    add border on navbar

--- src/app/(main)/components/Navbar/Navbar.jsx
+++ src/app/(main)/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full z-50 transition-all md:px-11 duration-300 ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-[#c45627] rounded-b-xs transition-all md:px-11 duration-300 ${
         scrolled
           ? "backdrop-blur-md bg-base-100/70 shadow-sm"
           : "bg-base-100"

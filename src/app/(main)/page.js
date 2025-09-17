10f99fc525b2b56dd7d613594afc99cdfd9a2f08
Niloy-Modak <niloymodaK42@gmail.com>
2025-09-17 18:16:43 +0600

    Featurs and Work Section

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,26 +1,18 @@
 import SynchroFeatures from "./components/FeaturesOfSynchor/SynchroFeatures";
 import SynchroWorks from "./components/HowSynchroWorks/SynchroWorks";
-import Banner from "./components/Banner/Banner";
-import Contact from "./components/Contact/Contact";
 import PopularTopic from "./components/popularTopic/PopularTopic";
 
 export default function Home() {
   return (
     <section>
       {/* banner */}
-      <Banner />
 
-      {/* Features of Synchor */}
-      <SynchroFeatures />
-
-      {/* How synchor works */}
-      <SynchroWorks />
-
+      {/*Features of Synchor */}
+      <SynchroFeatures/>
+      {/* How synchro works */}
+      <SynchroWorks/>
       {/* PopularTopic */}
-      <PopularTopic />
-
-      {/* contact page */}
-      <Contact />
+      <PopularTopic/>
     </section>
   );
 }

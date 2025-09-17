3f9ad9c7adb6d80c279be8107d38f4dfcc8f2d68
sudiptodas153 <sudiptodas4093@gmail.com>
2025-09-17 11:42:55 +0600

    some update

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import PopularTopic from "./(main)/popularTopic/page";
 export default function Home() {
   return (
    <div>
-    <h2>Working</h2>
+    <h2 className="text-center text-4xl font-bold">.....Working....</h2>
      <PopularTopic/>
    </div>
   );

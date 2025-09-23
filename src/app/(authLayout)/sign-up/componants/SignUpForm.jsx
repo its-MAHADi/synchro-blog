14ea7f2ba9aca779882f94b3ae825c86421964e3
sudiptodas153 <sudiptodas4093@gmail.com>
2025-09-23 21:48:47 +0600

    add profile section

--- src/app/(authLayout)/sign-up/componants/SignUpForm.jsx
+++ src/app/(authLayout)/sign-up/componants/SignUpForm.jsx
@@ -14,7 +14,7 @@ export default function SignUpForm() {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const password = form.password.value; 
+    const password = form.password.value;
 
     try {
       const result = await registerUser({ name, email, password });
@@ -27,7 +27,7 @@ export default function SignUpForm() {
           showConfirmButton: false,
         });
         setTimeout(() => {
-          router.push('/sign-in');
+          router.push('/signupCoverProfile');
         }, 2000);
       }
     } catch (error) {

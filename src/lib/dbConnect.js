006159e059ae9165807f3287ffd2285009d28405
its-MAHADi <mhkhandakar57@gmail.com>
2025-09-30 02:41:27 +0600

    Admin Dashboard Done

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,24 +1,35 @@
+
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 export const collectionNameObj = {
-  usersCollection : 'users',
-  eventCollection : 'events',
-  blogCollection : 'blogs',
-  followerCollection : 'followers',
-  featuresCollection : 'features',
-}
-
-export default function dbConnect(collectionName){
-
+  usersCollection: 'users',
+  eventCollection: 'events',
+  blogCollection: 'blogs',
+  followerCollection: 'followers',
+  featuresCollection: 'features',
+};
 
 const uri = process.env.MONGODB_URI;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+const dbName = process.env.DB_NAME;
+
+let client;
+let clientPromise;
+
+if (!uri) throw new Error("Please add your MongoDB URI to .env");
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1 } });
+    global._mongoClientPromise = client.connect();
   }
-}); 
-return client.db(process.env.DB_NAME).collection(collectionName);
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1 } });
+  clientPromise = client.connect();
+}
+
+export default async function dbConnect(collectionName) {
+  const client = await clientPromise;
+  const db = client.db(dbName);
+  return db.collection(collectionName);
 }

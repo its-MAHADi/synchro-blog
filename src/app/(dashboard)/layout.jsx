0df6aaf2b249c70544a5ad0c4ee9b2fb8f1fdaf6
its-MAHADi <mhkhandakar57@gmail.com>
2025-10-06 12:14:58 +0600

    user dashboard updated

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -12,11 +12,11 @@ export default function DashboardLayout({ children }) {
   return (
     <>
       <div className="md:flex min-h-screen">
-        {/* Sidebar /}
+        {/* Sidebar /} */}
         {role === "admin" ? <AdminSidebar /> : <UserSidebar />}
 
 
-        {/ Main Content */}
+         {/* Main Content */}
         <div className="flex-1  bg-gray-50">
             <div className="my-5 mx-5 ml-260">
               <Link href="/" className="btn border-[#c45627] text-[#c45627] text-xl font-bold hover:bg-[#c45627] hover:text-white rounded-sm">Home</Link>
